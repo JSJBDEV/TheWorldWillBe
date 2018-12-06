af8eb7f90813b609af33c96725ed63b570fd7039
JSJBDEV <acrodynasty@gmail.com>
2018-12-06 21:36:34 +0000

    values can be updated in the simulation
    gladly, i can access the entire towns "class" list from  another file, this means that actions can be passed through php to the run_server file that can then modify the data from the main generator class

--- Multiplay (PYPHPJS)/run_server.py
+++ Multiplay (PYPHPJS)/run_server.py
@@ -1,21 +1,21 @@
 import mysql.connector, pygen_server
+global database, cursor
 database = mysql.connector.connect(host="localhost",user="root",passwd="root",database="twwb")
+cursor = database.cursor()
+pygen_server.serverYear(100)
 
 
-def addTown(town):
-    cursor = database.cursor()
-    sql = "INSERT INTO towns (realName,realCountry,realRegion,foundedYear,latitude,longitude,townseed,happiness,devLevel,culture,military,population,foodMod,resources,branchedFrom,partOf) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-    val = (town["realName"],town["realCountry"],town["realRegion"],town["foundedYear"],town["latitude"],town["longitude"],town["townseed"],town["happiness"],town["devLevel"],town["culture"],town["military"],town["population"],town["foodMod"],town["resources"],town["branchedFrom"],town["partOf"])
-    cursor.execute(sql,val)
+def updateValue(town,modifiable,modifier):
+    town[modifiable]=modifier
+    sql = "UPDATE towns SET "+modifiable+"=%s WHERE realName=%s"
+    value = (modifier,town["realName"])
+    cursor.execute(sql,value)
     database.commit()
+    
+
+
+t = pygen_server.getTownByRealName("dar ben mbarek")
+updateValue(t,"resources",999)
 
 
 
-
-yeartasks = pygen_server.serverYear(4)
-while len(yeartasks) > 0:
-    if yeartasks[0] == ">":
-        yeartasks.pop(0)
-        addTown(yeartasks[0])
-        yeartasks.pop(0)
-    if yeartasks[0] == "#":
