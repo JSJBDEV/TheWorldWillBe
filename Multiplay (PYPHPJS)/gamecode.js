d0ceb530faf4540e8ab42ae95836a3de58f65f4d
JSJBDEV <acrodynasty@gmail.com>
2018-12-07 22:27:32 +0000

    added frontend bits for server

---
+++ Multiplay (PYPHPJS)/gamecode.js
@@ -0,0 +1,115 @@
+var markers = [];
+var polygons = [];
+var polylines = [];
+var map = L.map('map', {
+	minZoom: 1,
+	maxZoom: 16,
+	renderer: L.svg()  
+});
+var cartodbAttribution = '&copy; <a href="https://earthobservatory.nasa.gov/">NASA</a>';
+
+var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 1,
+	maxZoom: 16,
+	ext: 'jpg'
+}).addTo(map);
+
+var myIcon = L.icon({
+	//#d=11&c1=7&c2=7&c3=7&o=11&c4=7&s=17&c5=7 (highest values)
+	iconUrl: 'http://flag-designer.appspot.com/gwtflags/SvgFileService?d=0&c1=5&c2=3&c3=6&o=2&c4=5&s=13&c5=1',
+	iconSize: [60,40],
+	iconAnchor: [30, 20],
+	popupAnchor: [0, -5],
+	});
+	
+var subTownIcon = L.icon({
+	iconUrl: 'http://clipart.nicubunu.ro/svg/rpg_map/farm.svg',
+	iconSize: [60,50],
+	iconAnchor: [30, 20],
+	popupAnchor: [0, -5],});
+	
+var mainTownIcon = L.icon({
+	iconUrl: 'http://clipart.nicubunu.ro/svg/rpg_map/fort.svg',
+	iconSize: [60,50],
+	iconAnchor: [30, 20],
+	popupAnchor: [0, -5],});
+
+
+map.setView([0, 0], 0);
+
+ L.marker([-10.4333333,105.6833333]).addTo(map)
+.bindPopup('<a href="https://earthobservatory.nasa.gov/">NASA</a>');
+
+var latlngs = [[37, -109.05],[41, -109.03],[41, -102.05],[37, -102.04]];
+var polygon = L.polygon(latlngs, {color: 'red'}).addTo(map);
+
+//funcions by me//
+function addPolygon(latlngs,colour)
+{
+	var polygon = L.polygon(latlngs, {color: colour}).addTo(map);
+	polygons.push(polygon);
+	return polygon;
+	
+}
+function addPolyline(townF,latlngs,colour)
+{
+	var polyline = L.polyline(latlngs, {color: colour, townFrom: townF}).addTo(map);
+	polylines.push(polyline)
+	
+}
+function removePolyline(id)
+{
+	map.removeLayer(polylines[id]);
+	polylines.splice(id);
+}
+function removePolygon(id)
+{
+	map.removeLayer(polygons[id]);
+	polygons.splice(id);
+}
+
+function addMarker(name,origin,iconIn,lat,lon,desc)
+{
+	var marker = L.marker([lat,lon],{icon: iconIn, title:name, torigin:origin}).addTo(map).bindPopup(desc);
+	markers.push(marker);
+	return markers;
+}
+function removeMarker(id)
+{
+	map.removeLayer(markers[id]);
+	markers.splice(id);
+}
+
+
+
+//so basically, this script has almost nothing to do with display.js as it has to interperate towns differently
+
+fileStore = [];
+function getFile(url) //using a simple fetch method I can grab my libraries.
+{
+	document.getElementById("loads").innerHTML = "loading file, please wait....";
+	fetch(url)
+  .then(function(response) {
+    return response.text();
+  }).then(function(text) { 
+	fileStore = JSON.parse(text);
+	document.getElementById("loads").innerHTML = "file loaded";
+	return fileStore;
+});
+}
+
+function profile()
+{
+	document.getElementById("map").setAttribute("hidden",true);
+	if(fileStore.length < 1)
+	{
+		document.getElementById("otherdata").innerHTML = "<iframe src='twwb-game.php?action=signin&userId=3'></iframe>"
+	}
+}
+function getMap()
+{
+	document.getElementById("map").removeAttribute("hidden");
+	
+}
